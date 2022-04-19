9fe8e879709e89e3e6435e15495706dfb4fbf04f
Dr-Jones13 <93029483+Dr-Jones13@users.noreply.github.com>
2022-04-18 23:09:50 -0600

    updated serial_out.py
    added more stuff to test at lab tomorrow

--- Python/Jetson/Serial/Serial_Out.py
+++ Python/Jetson/Serial/Serial_Out.py
@@ -6,7 +6,7 @@ TCP_IP_SERVER = '0.0.0.0'  # IP of the server
 s1, s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "test!"
+
 
 # make sure to unblock firewall communication if need be
 
@@ -15,9 +15,9 @@ MESSAGE = "test!"
 
 def serial_out(data):
     s1.connect((TCP_IP_CLIENT, TCP_PORT))
+
     # testing a quick message
-    s1.send(MESSAGE)
-    data = s1.recv(BUFFER_SIZE)
+    s1.send(bytes("test!", "utf-8"))
     s1.close()
     # data will be
     # [[x, y, type, color], [x, y, type, color], [x, y, type, color]]
@@ -31,14 +31,13 @@ def serial_in():
 
     s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s2.bind((TCP_IP_SERVER, TCP_PORT))
-    s2.listen(1)
+    s2.listen(4)
 
     conn, addr = s2.accept()
     print('Connection address:', addr)
     while 1:
-        received_data = conn.recv(BUFFER_SIZE)
-        if not received_data:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
             break
-        print("received data:", received_data)
-        conn.send(received_data)  # echo
+        print("received data: ", data.decode("utf-8"))
     conn.close()

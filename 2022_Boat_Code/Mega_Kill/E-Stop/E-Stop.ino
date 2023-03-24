fc3e31134949039c6b7fed923650ae783f1d3889
Superkh <noobykyle@gmail.com>
2023-03-24 06:08:57 -0600

    Recoded E-Stop
    Changed how the E-Stop code on MegaKill runs.
    -waits for receiver connection
    -turns on boat
    -if more than 0.5s of disconnect kill
    -if button kill

--- 2022_Boat_Code/Mega_Kill/E-Stop/E-Stop.ino
+++ 2022_Boat_Code/Mega_Kill/E-Stop/E-Stop.ino
@@ -15,77 +15,70 @@
 #define Hard_Stop Radio::AUX1
 
 Radio rad;
-
-int StopPin = 7;
-int relay = 6;
-int hult_pin = 13;
+//used to store result from radio
 int input = 0;
 
-double dissconect_time = 5; //in mins
-long dissconnect_time_milli;
-double hult_time = 0.5; //in mins
-long hult_time_milli;
+
+int RadioStopPin = 7;
+int relay_KillPin = 6;
+
+//time for signal issues with radio before preminate disconect
+double dissconect_time = 0.5; //in sec
+long dissconnect_time_milli = (long)dissconect_time*1000;
+
+
 long last_connected_time;
-bool hult_state = false;
 
 
 void setup() {
-  pinMode(relay, OUTPUT); //enables relay
-  pinMode(hult_pin, OUTPUT); //enables relay
-  digitalWrite(hult_pin, LOW);
-  
+  pinMode(relay_KillPin, OUTPUT); //pinmode for Relay
+  digitalWrite(relay_KillPin, LOW); //off by defult
+
   Serial.begin(9600);
-  if ( rad.set(Hard_Stop, StopPin)==0) 
-  { //If radio pin is valid
-    //Gives power to the main relay
-    digitalWrite(relay,HIGH); 
-  }
-  else
+
+  Serial.println("Startup");
+  
+  
+  while( rad.set(Hard_Stop, RadioStopPin)!=0 )
   { //If the radio did not get a valid pin
     //Makes sure the power to the main relay is cut
-    digitalWrite(relay, LOW);
+    digitalWrite(relay_KillPin, LOW);
   }
+
+  Serial.println("Detected reciver");
+
+  //radio connected
+  //Give power to the main relay
+  digitalWrite(relay_KillPin,HIGH);
   
   last_connected_time = millis();
-  dissconnect_time_milli = (long)(dissconect_time*60*1000);
-  hult_time_milli = (long)(hult_time*60*1000);
+
+  
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println(rad.read(Hard_Stop));
   input = rad.read(Hard_Stop);
+  Serial.print("Input: ");
   Serial.println(input);
-
-  //Note: if radio gets disconneted the code does not do anything
-  //TODO: need to be fixed!
   
-  if (input!=0 && input!=-256) // true or pressed .read return 0 on high 
+  if (input!=0 && input!=-256) // Pressed kill button on radio
   {
-    digitalWrite(relay, LOW);    
+
+    Serial.println("Kill Button Pressed");
+    digitalWrite(relay_KillPin, LOW);    
   }
-  else if (input==-256) //disconnected
+  else if (input==-256) //disconnected form radio breafly
   { 
-    //hult
-    if (millis() - last_connected_time >= hult_time_milli)
-    {
-      hult_state = true;
-      digitalWrite(hult_pin, HIGH);
-    }
-
-
-    //kill
+    //kill (radio desconected to long)
     if (millis() - last_connected_time >= dissconnect_time_milli)
     {
-      digitalWrite(relay, LOW);
+      Serial.println("Time out");
+      digitalWrite(relay_KillPin, LOW);
     }
+    
   }
-  else if (hult_state && input==0)
-  {
-    hult_state = false;
-    digitalWrite(hult_pin, LOW);
-  }
-  else
+  else //kill not hit and still connected to radio
   {
     last_connected_time = millis();
   }
